d707ee55bc985585a7c24a71ece4346f817b18d6
Anthony Horton <anthony.horton@aao.gov.au>
2020-07-04 17:40:21 +1000

    Refactored lights control, added feeder

--- CIRCUITPY/code.py
+++ CIRCUITPY/code.py
@@ -11,15 +11,19 @@ from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
 from adafruit_ds18x20 import DS18X20
 from adafruit_featherwing import minitft_featherwing
+from adafruit_motor import stepper
+from adafruit_motorkit import MotorKit
 from adafruit_onewire.bus import OneWireBus
 from adafruit_pcf8523 import PCF8523
 from adafruit_sdcard import SDCard
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 # Operation settings
-LIGHTS_ON_TIME = (08, 00, 00)  # Time in (HH, mm, ss) format
-LIGHTS_OFF_TIME = (20, 00, 00)  # Time in (HH, mm, ss) format
+LIGHTS_ON_TIME = (02, 01, 00)  # Time in (HH, mm, ss) format
+LIGHTS_OFF_TIME = (02, 21, 30)  # Time in (HH, mm, ss) format
+FEEDING_TIMES = ((02, 06, 00), (02, 21, 00))
+PORTIONS_PER_MEAL = 2
 LOG_DATA = True
 LOG_INTERVAL = (00, 05, 00)  # Time in (HH, mm, ss) format
 BLE_NAME = "Scales Aquarium"
@@ -29,12 +33,24 @@ HEARTBEAT_PIN = board.RED_LED
 HEARTBEAT_DURATION = 0.02  # seconds
 LIGHTS_ENABLE_PIN = board.D11
 LIGHTS_DISABLE_PIN = board.D12
+FEEDER_MOTOR = "stepper1"
+FEEDER_STEPS_PER_ROTATION = 513
+FEEDER_STEP_DELAY = 0.01
+FEEDER_STEP_STYLE = stepper.DOUBLE
 OW_PIN = board.D13
 WATER_SN = b'\x28\xaa\x87\xed\x37\x14\x01\xee'
 WATER_OFFSET = 0.0  # Temperature sensor zero point offset in degrees C
 AIR_SN = b'\x28\x11\x8d\x97\x0a\x00\x00\x58'
 AIR_OFFSET = 0.0  # Temperature sensor zero point offset in degrees C
 SD_CS = board.D10
+
+
+def time_tuple_to_secs(time_tuple):
+    return time_tuple[0] * 3600 + time_tuple[1] * 60 + time_tuple[2]
+
+
+def time_struct_to_secs(time_struct):
+    return time_struct.tm_hour * 3600 + time_struct.tm_min * 60 + time_struct.tm_sec
 
 
 class LatchingRelay:
@@ -44,18 +60,148 @@ class LatchingRelay:
         self._set.direction = digitalio.Direction.OUTPUT
         self._unset = digitalio.DigitalInOut(unset_pin)
         self._unset.direction = digitalio.Direction.OUTPUT
+        self.disable()
 
     def enable(self):
         # Pull set pin high for >10ms to set relay
         self._set.value = True
         time.sleep(0.02)
         self._set.value = False
+        self.is_enabled = True
 
     def disable(self):
         # Pull unset pin high for >10ms to unset relay
         self._unset.value = True
         time.sleep(0.02)
         self._unset.value = False
+        self.is_enabled = False
+
+
+class Lights(LatchingRelay):
+    def __init__(self, lights_on_time, lights_off_time, set_pin, unset_pin):
+        super().__init__(set_pin, unset_pin)
+        self.on_time = lights_on_time
+        self.off_time = lights_off_time
+        self.update()
+
+    @property
+    def on_time(self):
+        return self._on_time
+
+    @on_time.setter
+    def on_time(self, on_time):
+        self._on_time = on_time
+        self._on_secs = time_tuple_to_secs(on_time)
+
+    @property
+    def off_time(self):
+        return self._off_time
+
+    @off_time.setter
+    def off_time(self, off_time):
+        self._off_time = off_time
+        self._off_secs = time_tuple_to_secs(off_time)
+
+    def update(self):
+        # Note, for now this logic assumes the lights off time is later in the day than lights
+        # on time, i.e. lights on period does not span midnight.
+        now = time.localtime()
+        secs = time_struct_to_secs(now)
+        if secs > self._on_secs and secs < self._off_secs:
+            if not self.is_enabled:
+                self.enable()
+                print("Lights turned on.")
+        if secs < self._on_secs or secs > self._off_secs:
+            if self.is_enabled:
+                self.disable()
+                print("Lights turned off.")
+
+
+class StepperMotor:
+    def __init__(self, motor, steps_per_rotation, step_delay, step_style, i2c):
+        self._kit = MotorKit(i2c=i2c)
+        self._stepper = getattr(self._kit, motor)
+        self._stepper.release()
+        self._steps_per_rotation = int(steps_per_rotation)
+        self._delay = step_delay
+        assert step_style in {stepper.SINGLE, stepper.DOUBLE}  # Don't want to microstep this geared motor
+        self._style = step_style
+
+    def rotate(self, n_rotations):
+        self.move(n_rotations * self._steps_per_rotation)
+
+    def move(self, n_steps):
+        n_steps = int(n_steps)
+        if n_steps > 0:
+            direction = stepper.FORWARD
+        elif n_steps < 0:
+            direction = stepper.BACKWARD
+        else:
+            return
+        try:
+            for i in range(abs(n_steps)):
+                self._stepper.onestep(direction=direction, style=self._style)
+                time.sleep(self._delay)
+        finally:
+            # De-energise the coils
+            self._stepper.release()
+
+
+class Feeder(StepperMotor):
+    def __init__(self, feeding_times, portions_per_meal, motor, steps_per_rotation, step_delay, step_style, i2c):
+        super().__init__(motor, steps_per_rotation, step_delay, step_style, i2c)
+        self.feeding_times = feeding_times
+        self.portions_per_meal = portions_per_meal
+        self._meals_fed_today = 0
+        now = time.localtime()
+        secs = time_struct_to_secs(now)
+        self._last_update = secs
+        for fs in self._feeding_secs:
+            if secs > fs:
+                self._meals_fed_today += 1
+
+    @property
+    def feeding_times(self):
+        return self._feeding_times
+
+    @feeding_times.setter
+    def feeding_times(self, times):
+        self._n_times = len(times)
+        self._feeding_times = times
+        self._feeding_secs = tuple(time_tuple_to_secs(t) for t in times)
+
+    @property
+    def portions_per_meal(self):
+        return self._portions_per_meal
+
+    @portions_per_meal.setter
+    def portions_per_meal(self, n_portions):
+        n_portions = int(n_portions)
+        assert n_portions > 0
+        self._portions_per_meal = n_portions
+
+    def feed(self, n_portions=1):
+        n_portions = int(n_portions)
+        assert n_portions > 0
+        self.rotate(n_portions)
+        print(f"Fed {n_portions} portions.")
+
+    def update(self):
+        now = time.localtime()
+        secs = time_struct_to_secs(now)
+        if secs < self._last_update:
+            # Midnight has past since last update
+            self._meals_fed_today = 0
+        self._last_update = secs
+        try:
+            next_feed = self._feeding_secs[self._meals_fed_today]
+        except IndexError:
+            # Done with feeding for today
+            return
+        if secs > next_feed:
+            self.feed(self.portions_per_meal)
+            self._meals_fed_today += 1
+            print(f"Fed meal {self._meals_fed_today} of {self._n_times}.")
 
 
 class TemperatureSensor:
@@ -83,53 +229,39 @@ class TemperatureSensor:
         raise RuntimeError("Too many errors attempting to read temperature sensor.")
 
 class Aquarium:
-    def __init__(self,
-                 lights_on_time=LIGHTS_ON_TIME,
-                 lights_off_time=LIGHTS_OFF_TIME,
-                 log_data=LOG_DATA,
-                 log_interval=LOG_INTERVAL,
-                 heartbeat_pin=HEARTBEAT_PIN,
-                 heartbeat_duration=HEARTBEAT_DURATION,
-                 lights_enable_pin=LIGHTS_ENABLE_PIN,
-                 lights_disable_pin=LIGHTS_DISABLE_PIN,
-                 ow_pin=OW_PIN,
-                 water_sn=WATER_SN,
-                 water_offset=WATER_OFFSET,
-                 air_sn=AIR_SN,
-                 air_offset=AIR_OFFSET):
+    def __init__(self):
         # Set up heartbeat output (i.e red LED)
         self._heartbeat = digitalio.DigitalInOut(HEARTBEAT_PIN)
         self._heartbeat.direction = digitalio.Direction.OUTPUT
-        self._heartbeat_duration = heartbeat_duration
+        self._heartbeat_duration = HEARTBEAT_DURATION
 
-        # Set up latching relay for lights
-        self._lights_on_secs = self.time_tuple_to_secs(lights_on_time)
-        self._lights_off_secs = self.time_tuple_to_secs(lights_off_time)
-        self._lights_relay = LatchingRelay(set_pin=lights_enable_pin, unset_pin=lights_disable_pin)
-        self._lights_relay.disable()
-        self._lights_on = False
+        # Set up lights
+        self.lights = Lights(LIGHTS_ON_TIME, LIGHTS_OFF_TIME, LIGHTS_ENABLE_PIN, LIGHTS_DISABLE_PIN)
+
+        # Set up 1-Wire temperature sensors
+        ow_bus = OneWireBus(OW_PIN)
+        self.water_sensor = TemperatureSensor(ow_bus, WATER_SN, WATER_OFFSET)
+        self.air_sensor = TemperatureSensor(ow_bus, AIR_SN, AIR_OFFSET)
 
         # Set up I2C bus
         i2c = busio.I2C(board.SCL, board.SDA)
+
+        # Set up feeder
+        self.feeder = Feeder(FEEDING_TIMES, PORTIONS_PER_MEAL, FEEDER_MOTOR, FEEDER_STEPS_PER_ROTATION,
+                             FEEDER_STEP_DELAY, FEEDER_STEP_STYLE, i2c)
 
         # Set up real time clock as source for time.time() or time.localtime() calls.
         clock = PCF8523(i2c)
         rtc.set_time_source(clock)
 
-        # Set up 1-Wire temperature sensors
-        ow_bus = OneWireBus(board.D13)
-        self._water_sensor = TemperatureSensor(ow_bus, water_sn, water_offset)
-        self._air_sensor = TemperatureSensor(ow_bus, air_sn, air_offset)
-
         # Set up SD card
-        displayio.release_displays()
         spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
         cs = digitalio.DigitalInOut(SD_CS)
         sdcard = SDCard(spi, cs)
         vfs = storage.VfsFat(sdcard)
         storage.mount(vfs, "/sd")
-        self._log_data = log_data
-        self._log_interval = self.time_tuple_to_secs(log_interval)
+        self._log_data = LOG_DATA
+        self._log_interval = time_tuple_to_secs(LOG_INTERVAL)
         self._last_log = None
 
         # Set up display
@@ -141,39 +273,20 @@ class Aquarium:
         self._ble_uart = UARTService()
         self._ble_ad = ProvideServicesAdvertisement(self._ble_uart)
 
-    def time_tuple_to_secs(self, time_tuple):
-        return time_tuple[0] * 3600 + time_tuple[1] * 60 + time_tuple[2]
-
-    def time_struct_to_secs(self, time_struct):
-        return time_struct.tm_hour * 3600 + time_struct.tm_min * 60 + time_struct.tm_sec
-
     def heartbeat(self):
         self._heartbeat.value = True
         time.sleep(self._heartbeat_duration)
         self._heartbeat.value = False
 
-    def update_lights(self):
-        # Note, for now this logic assumes the lights off time is later in the day than lights
-        # on time, i.e. lights on period does not span midnight.
-        secs = self.time_tuple_to_secs((self._now.tm_hour, self._now.tm_min, self._now.tm_sec))
-        if secs > self._lights_on_secs and secs < self._lights_off_secs:
-           if not self._lights_on:
-               self._lights_relay.enable()
-               self._lights_on = True
-        if secs < self._lights_on_secs or secs > self._lights_off_secs:
-            if self._lights_on:
-                self._lights_relay.disable()
-                self._lights_on = False
-
     def update_temps(self):
-        self._water_temp = self._water_sensor.temperature
-        self._air_temp = self._air_sensor.temperature
+        self.water_temp = self.water_sensor.temperature
+        self.air_temp = self.air_sensor.temperature
 
     def update_display(self):
         time_string = "Time:   %d:%02d:%02d\n" % (self._now.tm_hour, self._now.tm_min, self._now.tm_sec)
-        lights_string = "Lights: {}\n".format("On" if self._lights_on else "Off")
-        water_string = "Water:  {:7.4f}C\n".format(self._water_temp)
-        air_string = "Air:    {:7.4f}C\n".format(self._air_temp)
+        lights_string = "Lights: {}\n".format("On" if self.lights.is_enabled else "Off")
+        water_string = "Water:  {:7.4f}C\n".format(self.water_temp)
+        air_string = "Air:    {:7.4f}C\n".format(self.air_temp)
         output = time_string + lights_string + water_string + air_string
         print(output)
 
@@ -182,8 +295,8 @@ class Aquarium:
             return
 
         if self._last_log:
-            last_log_secs = self.time_struct_to_secs(self._last_log)
-            current_secs = self.time_struct_to_secs(self._now)
+            last_log_secs = time_struct_to_secs(self._last_log)
+            current_secs = time_struct_to_secs(self._now)
             if current_secs - last_log_secs < self._log_interval:
                 return
 
@@ -219,7 +332,8 @@ class Aquarium:
     def run_once(self):
         self.heartbeat()
         self._now = time.localtime()
-        self.update_lights()
+        self.lights.update()
+        self.feeder.update()
         self.update_temps()
         self.update_display()
         self.update_log()
@@ -245,9 +359,10 @@ class Aquarium:
     def _get_status_strings(self):
         datestamp = "{:04d}-{:02d}-{:02d}".format(self._now.tm_year, self._now.tm_mon, self._now.tm_mday)
         timestamp = datestamp + "T{:02d}:{:02d}:{:02d}".format(self._now.tm_hour, self._now.tm_min, self._now.tm_sec)
-        status = "{}, {:d}, {:7.4f}, {:7.4f}".format(timestamp, self._lights_on, self._water_temp, self._air_temp)
+        status = "{}, {:d}, {:7.4f}, {:7.4f}".format(timestamp, self.lights.is_enabled, self.water_temp, self.air_temp)
         return datestamp, timestamp, status
 
 
+displayio.release_displays()
 aquarium = Aquarium()
 aquarium.run()
